c98e19dd588446f50dcc7338c5fab23041d0d529
Ochkalov <ochkalov@gmail.com>
2017-06-11 20:19:33 +0300

    old

---
+++ goitHW/js_1-2/js/test.js
@@ -0,0 +1,71 @@
+/*var vasya = {
+    name: 'Vasya',
+    greetings: function() {
+// Insert your code here
+//console.log('My name is ' + name);
+//console.log('My name is ' + this.name);
+//console.log('My name is '+ this);
+    }
+};        
+vasya.greetings();*/
+
+
+/*var obj = {a: 1};        
+var increment = function(obj){ 
+    obj = {a: 2} 
+}        
+increment(obj);        
+console.log(obj.a);*/
+
+/*var obj = {a: 1};       
+var increment = function(obj){ obj.a++ }        
+increment(obj);        
+console.log(obj.a);*/
+
+/*var a = 1; 
+var increment = function(a) { a++ };
+increment(a);
+console.log(a);*/
+
+
+
+//console.log(1 == '1');
+
+
+
+/*var result = 5 + '5' - 0;
+console.log(result);
+console.log(typeof result);*/
+
+/*
+var aString = {value: 'Im a string!!!'};
+console.log(typeof aString);
+*/
+
+/*
+var num = parseInt('12');
+console.log(num);
+var num = new Number('12');
+console.log(typeof(num));
+*/
+
+
+/*
+var arr = [1,2,3];
+arr[3] = 4;
+console.log(arr.length);
+console.log(arr);
+*/
+
+
+	
+/*
+var obj = {prop1: 1, prop2: 2};
+console.log(obj[prop1]);
+*/
+
+
+/*var str = 'string';
+//console.log(str[0] = 'S');
+str[0] = 'S';
+console.log(str[0]);*/
